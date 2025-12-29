f0c963eeaf0cb935159515a2ac6d45cd803886ad
LXSCA <lucas.edr27@gmail.com>
2025-12-29 19:53:48 -0300

    feat: creating web server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello, world! =>")
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("pong\n"))
+	})
+
+	port := ":8080"
+	fmt.Printf("🚀 api running at %s\n\n", port)
+	http.ListenAndServe(port, mux)
 }
