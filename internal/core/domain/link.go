898dbdd289c38c17d60c1087d7469e81dbedbe94
LXSCA <lucas.edr27@gmail.com>
2025-12-31 12:51:29 -0300

    fix: fix typo ShortCOde -> ShortCode

--- internal/core/domain/link.go
+++ internal/core/domain/link.go
@@ -5,7 +5,7 @@ import "time"
 type Link struct {
 	Id          int64     `json"id,string"`
 	OriginalURL string    `json:"original_url"`
-	ShortCOde   string    `json:"short_code"`
+	ShortCode   string    `json:"short_code"`
 	CreatedAt   time.Time `json:"created_at"`
 	Visits      int       `json:"visits"`
 }
