a2969dc19a894cecbba5d6b1588f9ea43b70eb48
LXSCA <lucas.edr27@gmail.com>
2025-12-31 13:53:21 -0300

    feat: constructor func

--- internal/adapters/idgen/snowflake.go
+++ internal/adapters/idgen/snowflake.go
@@ -9,6 +9,17 @@ type SnowflakeAdapter struct {
 	node *snowflake.Node
 }
 
+func NewSnowflakeIDGen(nodeID int64) (*SnowflakeAdapter, error) {
+	node, err := snowflake.NewNode(nodeID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &SnowflakeAdapter{
+		node: node,
+	}, nil
+}
+
 var _ ports.IDGenerator = (*SnowflakeAdapter)(nil)
 
 func (s *SnowflakeAdapter) Generate() int64 {
